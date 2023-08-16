81f11c9a3760401868611743bb265ef0226e245a
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-08-16 22:12:56 +0200

    build: add `@szum-tech/design-system` package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@szum-tech/design-system/{components,hooks,contexts}/**/*{js,ts,jsx,tsx}"
+  ],
   theme: {},
-  plugins: []
+  presets: [require("@szum-tech/design-system/theme/main-preset")]
 };
 export default config;

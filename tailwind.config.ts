9cf144170ceca115768a15cbdd676bbc8abe5cd6
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-08-18 14:41:17 +0200

    fix: fx tailwind css config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@szum-tech/design-system/{components,hooks,contexts}/**/*{js,ts,jsx,tsx}"
+    "./node_modules/@szum-tech/design-system/**/*{mjs,js}"
   ],
   theme: {},
   presets: [require("@szum-tech/design-system/theme/main-preset")]

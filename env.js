ad32f7b2d83f9a583c8a8d8fc999286fc76319ac
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-08-18 15:37:47 +0200

    refactor: refactor env file

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 const { createEnv } = require("@t3-oss/env-nextjs");
 const { z } = require("zod");
 
-const env = createEnv({
+export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     ANALYZE: z
@@ -23,5 +23,3 @@ const env = createEnv({
     CI: process.env.CI
   }
 });
-
-module.exports = { env };

2d18201b86071c74edb8cf68b0a91bb616384d7c
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-08-29 22:16:55 +0200

    build(deps): bump deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@szum-tech/design-system/**/*{mjs,js}"
+    "./node_modules/@szum-tech/design-system/dist/*{mjs,js}"
   ],
   theme: {},
   presets: [require("@szum-tech/design-system/theme/main-preset")]

1538b1393ff59a46c7d00a232b7513d294e14d59
JanSzewczyk <jan.szewczyk1997@gmail.com>
2024-05-27 22:09:02 +0200

    fix(package-config): update package to module type

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,9 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
-const reporter = process.env.CI ? ["json", "github-actions"] : ["text", "json", "html"];
+import { env } from "./env";
+
+const reporter = env.CI ? ["json", "github-actions"] : ["text", "json", "html"];
 
 // https://vitejs.dev/config/
 export default defineConfig({

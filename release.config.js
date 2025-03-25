de63317a8274b36a4beac59d3f16bbe3cbbf34d8
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-03-25 22:16:42 +0100

    build(release): improve `semantic-release` config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,1 @@
-/** @type {import('semantic-release').Options} */
-module.exports = {
-  branches: ["main"],
-  extends: "@szum-tech/semantic-release-preset/without-npm"
-};
+export { default } from "@szum-tech/semantic-release-config/without-npm";

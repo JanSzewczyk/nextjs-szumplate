40f86f6562ed47b44628f5d699ed7f6b7b8601aa
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-03-25 22:20:06 +0100

    build(next): update next config

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import { type NextConfig } from "next";
+import withPlugins from "next-compose-plugins";
+
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+import { env } from "./env";
+
+const config: NextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: "/healthz", destination: "/api/health" },
+      { source: "/api/healthz", destination: "/api/health" },
+      { source: "/health", destination: "/api/health" },
+      { source: "/ping", destination: "/api/health" }
+    ];
+  }
+};
+
+export default withPlugins([withBundleAnalyzer({ enabled: env.ANALYZE }), config]);

3d096e697f41e3c242799a1f69974492dccd9d0a
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-07-10 23:31:09 +0200

    fix(envs): fix env references

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { type MetadataRoute } from "next";
 
-import { env } from "~/env";
+import { env } from "~/data/env/server";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [{ url: `${env.VERCEL_URL}/`, lastModified: new Date().toISOString() }];

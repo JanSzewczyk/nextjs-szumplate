bdc0208569a68cfa1e1d07bdca4c070714860f5e
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2026-01-06 21:25:29 +0100

    refactor(icon): simplify JSX structure in Icon component

--- app/icon.tsx
+++ app/icon.tsx
@@ -9,22 +9,20 @@ export const contentType = "image/png";
 
 export default function Icon() {
   return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 16,
-          background: "#111111FF",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "#EAEAEAFF"
-        }}
-      >
-        NS
-      </div>
-    ),
+    <div
+      style={{
+        fontSize: 16,
+        background: "#111111FF",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#EAEAEAFF"
+      }}
+    >
+      NS
+    </div>,
     {
       ...size
     }

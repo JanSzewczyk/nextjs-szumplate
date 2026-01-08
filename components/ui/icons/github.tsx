fd52501ac0b4c322d4d3a8214e8b0c112bea6079
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2026-01-08 11:40:32 +0100

    refactor(icons): rename GitHubIcon to GithubIcon for consistency

--- components/ui/icons/github.tsx
+++ components/ui/icons/github.tsx
@@ -1,4 +1,4 @@
-export function GitHubIcon({ className }: { className?: string }) {
+export function GithubIcon({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"

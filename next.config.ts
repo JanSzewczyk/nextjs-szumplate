8bb4ac2e706f68ad5b32501cb00e385894b03a9c
JanSzewczyk <jan.szewczyk1997@gmail.com>
2026-06-30 17:52:22 +0200

    refactor: simplify next.config.ts and remove unused dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
-import withBundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
-import withPlugins from "next-compose-plugins";
 
-import { env } from "./data/env/server";
-
-const config: NextConfig = {
+export default {
   experimental: {
     optimizePackageImports: ["@szum-tech/design-system"]
+  },
+  logging: {
+    browserToTerminal: true
   },
   productionBrowserSourceMaps: true,
   reactCompiler: true,
   reactStrictMode: true,
-
   async rewrites() {
     return [
       { destination: "/api/health", source: "/healthz" },
@@ -19,8 +17,5 @@ const config: NextConfig = {
       { destination: "/api/health", source: "/health" },
       { destination: "/api/health", source: "/ping" }
     ];
-  },
-  serverExternalPackages: ["pino", "pino-pretty"]
-};
-
-export default withPlugins([withBundleAnalyzer({ enabled: env.ANALYZE }), config]);
+  }
+} satisfies NextConfig;
